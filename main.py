5e3aa0a1d180c2c80088240000a3fd4c5307d6d6
Jaskaran Bindra <jskrnbindra@gmail.com>
2018-06-19 12:06:15 +0530

    enhancement: accepting command line arguments with default

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from scrap_panda import ScrapPanda
+import sys
 
 
-if __name__ == '__main__':
+def get_parameters():
+    """
+    Accepts URL, sample product name and sample category as command line arguments.
+    Uses default if insufficient arguments provided.
+    """
     url = 'https://www.snapdeal.com'
     sample_product_name = 'Lenovo Ideapad 110 (80T70015IH) Notebook (Intel Pentium- 4GB RAM- 1TB HDD- 39.62cm (15.6)- DOS) (Black)'
     sample_category_name = 'Loafers'
 
+    if not len(sys.argv) > 2:
+        print('\nNo or insufficient command line arguments passed. Using default.\n')
+        print('url:', url)
+        print('sample_product_name:', sample_product_name)
+        print('sample_category_name:', sample_category_name + '\n')
+        return url, sample_product_name, sample_category_name
+
+    cla_url = sys.argv[1]
+    cla_sample_product_name = sys.argv[2]
+    cla_sample_category_name = sys.argv[3]
+
+    return cla_url or url, \
+           cla_sample_product_name or sample_product_name, \
+           cla_sample_category_name or sample_category_name
+
+
+if __name__ == '__main__':
+
+    inputs = get_parameters()
     scrapper = ScrapPanda()
-    products, categories = scrapper.start(url, sample_product_name, sample_category_name)
+    products, categories = scrapper.start(*inputs)
 
     print(products)
     print(categories)
